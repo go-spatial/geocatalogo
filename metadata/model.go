86574af041bfe914adf934159e8e3f44a305e207
Tom Kralidis <tomkralidis@gmail.com>
2017-12-01 14:22:38 +0000

    add default env, CSW Record parser, default Record model

---
+++ metadata/model.go
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// The MIT License (MIT)
+// Copyright (c) 2017 Tom Kralidis
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package metadata
+
+import (
+    "time"
+)
+
+type Keywords struct {
+    Keyword []string
+    Type string
+}
+
+type Contact struct {
+    Type string
+    Value string
+}
+
+type Date struct {
+    Type string
+    Value string
+}
+
+type Extent struct {
+    Spatial [4]float64 // minx, miny, maxx, maxy
+    Temporal [2]time.Time
+}
+
+type Link struct {
+    Name string
+    Description string
+    Protocol string
+    URL string
+}
+
+type Record struct {
+    Identifier string
+    Title string
+    Type string
+    DateInserted time.Time
+    DateModified time.Time
+    Schema string
+    Abstract string
+    KeywordsSets []Keywords
+    Contacts []Contact
+    Dates []Date
+    License string
+    Language string
+    Extents []Extent
+    Links []Link
+}
---
+++ metadata/parsers/csw_record.go
@@ -0,0 +1,92 @@
+package parsers
+
+import (
+  "bytes"
+  "encoding/xml"
+  "fmt"
+  "strconv"
+  "strings"
+
+  "golang.org/x/net/html/charset"
+
+  "github.com/tomkralidis/geocatalogo/metadata"
+)
+
+type CSWRecord struct {
+    Identifier string `xml:"http://purl.org/dc/elements/1.1/ identifier"`
+    Type string `xml:"http://purl.org/dc/elements/1.1/ type"`
+    Title string `xml:"http://purl.org/dc/elements/1.1/ title"`
+    Modified string `xml:"http://purl.org/dc/terms/ modified"`
+    Abstract string `xml:"http://purl.org/dc/terms/ abstract"`
+    Subject []string `xml:"http://purl.org/dc/elements/1.1/ subject"`
+    Format string `xml:"http://purl.org/dc/elements/1.1/ format"`
+    Creator string `xml:"http://purl.org/dc/elements/1.1/ creator"`
+    Language string `xml:"http://purl.org/dc/elements/1.1/ language"`
+    References []string `xml:"http://purl.org/dc/elements/1.1/ references"`
+    WGS84BoundingBox WGS84BoundingBox `xml:http://www.opengis.net/ows WGS84BoundingBox"`
+}
+
+type WGS84BoundingBox struct {
+    LowerCorner string `xml:"http://www.opengis.net/ows LowerCorner"`
+    UpperCorner string `xml:"http://www.opengis.net/ows UpperCorner"`
+}
+
+func (e *WGS84BoundingBox) Minx() (float64, error) {
+    s := strings.Split(e.LowerCorner, " ")
+    minx, err := strconv.ParseFloat(s[0], 64)
+    if err != nil {
+        return -9999, err
+    }
+    return minx, nil
+}
+
+func (e *WGS84BoundingBox) Miny() (float64, error) {
+    s := strings.Split(e.LowerCorner, " ")
+    miny, err := strconv.ParseFloat(s[1], 64)
+    if err != nil {
+        return -9999, err
+    }
+    return miny, nil
+}
+
+func (e *WGS84BoundingBox) Maxx() (float64, error) {
+    s := strings.Split(e.UpperCorner, " ")
+    maxx, err := strconv.ParseFloat(s[0], 64)
+    if err != nil {
+        return -9999, err
+    }
+    return maxx, nil
+}
+
+func (e *WGS84BoundingBox) Maxy() (float64, error) {
+    s := strings.Split(e.UpperCorner, " ")
+    maxy, err := strconv.ParseFloat(s[1], 64)
+    if err != nil {
+        return -9999, err
+    }
+    return maxy, nil
+}
+
+func ParseCSWRecord(xmlBuffer []byte) (metadata.Record, error) {
+    var cswRecord CSWRecord
+    var metadataRecord metadata.Record
+    reader := bytes.NewReader(xmlBuffer)
+    decoder := xml.NewDecoder(reader)
+    decoder.CharsetReader = charset.NewReaderLabel
+
+    err := decoder.Decode(&cswRecord)
+
+    if err != nil {
+        fmt.Println("decoder error:", err)
+        return metadataRecord, err
+    }
+
+    metadataRecord = metadata.Record{
+        Identifier: cswRecord.Identifier,
+        Type: cswRecord.Type,
+        Title: cswRecord.Title,
+        Abstract: cswRecord.Abstract,
+    }
+
+    return metadataRecord, nil
+}
