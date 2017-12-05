386614a825a874a1653fbdb71254ed8ea4b99ac2
Tom Kralidis <tomkralidis@gmail.com>
2017-12-04 20:23:59 -0500

    use switch/case

--- config/config.go
+++ config/config.go
@@ -91,74 +91,78 @@ func LoadFromEnv() Config {
 	cfg.Repository.Mappings = make(map[string]string)
 	for _, e := range os.Environ() {
 		pair := strings.Split(e, "=")
-		if pair[0] == "GEOCATALOGO_SERVER_URL" {
+
+		switch pair[0] {
+		case "GEOCATALOGO_SERVER_URL":
 			cfg.Server.URL = pair[1]
-		} else if pair[0] == "GEOCATALOGO_SERVER_MIMETYPE" {
+		case "GEOCATALOGO_SERVER_MIMETYPE":
 			cfg.Server.MimeType = pair[1]
-		} else if pair[0] == "GEOCATALOGO_SERVER_ENCODING" {
+		case "GEOCATALOGO_SERVER_ENCODING":
 			cfg.Server.Encoding = pair[1]
-		} else if pair[0] == "GEOCATALOGO_SERVER_LANGUAGE" {
+		case "GEOCATALOGO_SERVER_LANGUAGE":
 			cfg.Server.Language = pair[1]
-		} else if pair[0] == "GEOCATALOGO_SERVER_PRETTY_PRINT" {
+		case "GEOCATALOGO_SERVER_PRETTY_PRINT":
 			cfg.Server.PrettyPrint, _ = strconv.ParseBool(pair[1])
-		} else if pair[0] == "GEOCATALOGO_SERVER_LIMIT" {
+		case "GEOCATALOGO_SERVER_LIMIT":
 			cfg.Server.Limit, _ = strconv.Atoi(pair[1])
-		} else if pair[0] == "GEOCATALOGO_LOGGING_LEVEL" {
+		case "GEOCATALOGO_LOGGING_LEVEL":
 			cfg.Logging.Level = pair[1]
-		} else if pair[0] == "GEOCATALOGO_LOGGING_LOGFILE" {
+		case "GEOCATALOGO_LOGGING_LOGFILE":
 			cfg.Logging.Logfile = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_IDENTIFICATION_TITLE" {
+		case "GEOCATALOGO_METADATA_IDENTIFICATION_TITLE":
 			cfg.Metadata.Identification.Title = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_IDENTIFICATION_ABSTRACT" {
+		case "GEOCATALOGO_METADATA_IDENTIFICATION_ABSTRACT":
 			cfg.Metadata.Identification.Abstract = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_IDENTIFICATION_KEYWORDS" {
+		case "GEOCATALOGO_METADATA_IDENTIFICATION_KEYWORDS":
 			cfg.Metadata.Identification.Keywords = strings.Split(pair[1], ",")
-		} else if pair[0] == "GEOCATALOGO_METADATA_IDENTIFICATION_KEYWORDS_TYPE" {
+		case "GEOCATALOGO_METADATA_IDENTIFICATION_KEYWORDS_TYPE":
 			cfg.Metadata.Identification.KeywordsType = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_IDENTIFICATION_FEES" {
+		case "GEOCATALOGO_METADATA_IDENTIFICATION_FEES":
 			cfg.Metadata.Identification.Fees = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_IDENTIFICATION_ACCESSCONSTRAINTS" {
+		case "GEOCATALOGO_METADATA_IDENTIFICATION_ACCESSCONSTRAINTS":
 			cfg.Metadata.Identification.AccessConstraints = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_PROVIDER_NAME" {
+		case "GEOCATALOGO_METADATA_PROVIDER_NAME":
 			cfg.Metadata.Provider.Name = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_PROVIDER_URL" {
+		case "GEOCATALOGO_METADATA_PROVIDER_URL":
 			cfg.Metadata.Provider.URL = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_NAME" {
+		case "GEOCATALOGO_METADATA_CONTACT_NAME":
 			cfg.Metadata.Contact.Name = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_POSITION" {
+		case "GEOCATALOGO_METADATA_CONTACT_POSITION":
 			cfg.Metadata.Contact.Position = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_ADDRESS" {
+		case "GEOCATALOGO_METADATA_CONTACT_ADDRESS":
 			cfg.Metadata.Contact.Address = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_CITY" {
+		case "GEOCATALOGO_METADATA_CONTACT_CITY":
 			cfg.Metadata.Contact.City = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_STATEORPROVINCE" {
+		case "GEOCATALOGO_METADATA_CONTACT_STATEORPROVINCE":
 			cfg.Metadata.Contact.StateOrProvince = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_POSTALCODE" {
+		case "GEOCATALOGO_METADATA_CONTACT_POSTALCODE":
 			cfg.Metadata.Contact.PostalCode = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_COUNTRY" {
+		case "GEOCATALOGO_METADATA_CONTACT_COUNTRY":
 			cfg.Metadata.Contact.Country = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_PHONE" {
+		case "GEOCATALOGO_METADATA_CONTACT_PHONE":
 			cfg.Metadata.Contact.Phone = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_FAX" {
+		case "GEOCATALOGO_METADATA_CONTACT_FAX":
 			cfg.Metadata.Contact.Fax = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_EMAIL" {
+		case "GEOCATALOGO_METADATA_CONTACT_EMAIL":
 			cfg.Metadata.Contact.Email = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_URL" {
+		case "GEOCATALOGO_METADATA_CONTACT_URL":
 			cfg.Metadata.Contact.URL = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_HOURS_OF_SERVICE" {
+		case "GEOCATALOGO_METADATA_CONTACT_HOURS_OF_SERVICE":
 			cfg.Metadata.Contact.Hours = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_CONTACT_INSTRUCTIONS" {
+		case "GEOCATALOGO_METADATA_CONTACT_INSTRUCTIONS":
 			cfg.Metadata.Contact.Instructions = pair[1]
-		} else if pair[0] == "GEOCATALOGO_METADATA_ROLE" {
+		case "GEOCATALOGO_METADATA_ROLE":
 			cfg.Metadata.Contact.Role = pair[1]
-		} else if pair[0] == "GEOCATALOGO_REPOSITORY_TYPE" {
+		case "GEOCATALOGO_REPOSITORY_TYPE":
 			cfg.Repository.Type = pair[1]
-		} else if pair[0] == "GEOCATALOGO_REPOSITORY_URL" {
+		case "GEOCATALOGO_REPOSITORY_URL":
 			cfg.Repository.URL = pair[1]
-		} else if strings.HasPrefix(pair[0], "GEOCATALOGO_REPOSITORY_MAPPINGS") {
-			tokens := strings.Split(pair[0], "GEOCATALOGO_REPOSITORY_MAPPINGS_")
-			key := strings.ToLower(tokens[1])
-			cfg.Repository.Mappings[key] = pair[1]
+		default:
+			if strings.HasPrefix(pair[0], "GEOCATALOGO_REPOSITORY_MAPPINGS") {
+				tokens := strings.Split(pair[0], "GEOCATALOGO_REPOSITORY_MAPPINGS_")
+				key := strings.ToLower(tokens[1])
+				cfg.Repository.Mappings[key] = pair[1]
+			}
 		}
 	}
 	return cfg
